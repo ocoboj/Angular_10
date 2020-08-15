5b9d065462714612f9a8fdab979cd97b04c290e6
Olga Conesa <olgaconesaboj@gmail.com>
2020-08-15 10:22:18 +0200

    Tipado del retorno de una función

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,43 +1,17 @@
 (() => {
 
-    /*     class Avenger {
-    
-            nombre: string;
-            equipo: string;
-            nombreReal?: string;
-    
-            puedePelear: boolean;
-            peleasGanadas: number;
-    
-            constructor(nombre: string, equipo: string) {
-                this.nombre = nombre;
-                this.equipo = equipo;
-            }
-        } */
+    const sumar = (a: number, b:number): number => a + b;
 
-    class Avenger {
+    const nombre = (): string => 'Hola Olga';
 
-        /* nombre: string;
-        equipo: string;
-        nombreReal?: string;
+    const obtenerSalario = (): Promise<string> => {
 
-        puedePelear: boolean;
-        peleasGanadas: number;
- */
-        constructor(
-            public nombre: string,
-            public equipo: string, 
-            public nombreReal?: string,
-            public puedePelear: boolean = true,
-            public peleasGanadas: number = 0
-             ) {
-
-        }
+        return new Promise(( resolve, reject) => {
+            resolve('Olga');
+        })
     }
 
-    const antman: Avenger = new Avenger('Antman', 'Capi');
-
-    console.log(antman);
+    obtenerSalario().then( a => console.log (a.toUpperCase()))
 
 })();
 
