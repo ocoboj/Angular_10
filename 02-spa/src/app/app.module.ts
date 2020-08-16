80bbde0bd1f3879447663b613a9b482e1426eaae
Olga Conesa <olgaconesaboj@gmail.com>
2020-08-16 12:27:42 +0200

    Rutas en Angular

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+
+
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Servicios
+
+//Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,7 +25,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
