892ff8a1acf16755117f3d88d2af56b090628a02
Olga Conesa <olgaconesaboj@gmail.com>
2020-08-25 09:09:36 +0200

    Buscador de Héroes

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 
 
@@ -28,7 +29,8 @@ import { HeroeComponent } from './components/heroe/heroe.component';
     HomeComponent,
     AboutComponent,
     HeroesComponent,
-    HeroeComponent
+    HeroeComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
