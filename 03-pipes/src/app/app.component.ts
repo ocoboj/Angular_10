a9b9b5494d42b615a8a66fb45a764009ea21f78b
Olga Conesa <olgaconesaboj@gmail.com>
2020-08-26 10:28:30 +0200

    Pipe: Data - Fecha

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   PI: number = Math.PI;
   porcentaje: number = 0.235;
   salario: number = 1234.5;
+  fecha: Date = new Date();
 
   valorPromesa = new Promise<string>( (resolve) => {
     setTimeout(()=> {
