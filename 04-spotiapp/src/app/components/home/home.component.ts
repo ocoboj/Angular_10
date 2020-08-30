a0580e7ea302408e0f93f263176c4f877c403c71
Olga Conesa <olgaconesaboj@gmail.com>
2020-08-30 12:48:19 +0200

    Consumiendo información del servicio spotify

--- 04-spotiapp/src/app/components/home/home.component.ts
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -7,12 +7,17 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class HomeComponent {
 
+  nuevasCanciones: any[] = [];
 
-  constructor( private spotify: SpotifyService) {
+  constructor(private spotify: SpotifyService) {
 
-    this.spotify.getNewReleases();
+    this.spotify.getNewReleases()
+    .subscribe((data: any) => {
+      console.log(data.albums.items);
+      this.nuevasCanciones = data.albums.items;
+    });
 
-   }
+  }
 
 
 }
--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -11,13 +11,12 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
+
       'Authorization': 'Bearer BQDgkhpveX4MyFTi3qBCKeK57kQJUrmMCoG1z1wo7gmAK6li_MT1cIy9CkxBq1n1WQB3XxR5BaiaZ1ZOU5E'
+
     });
 
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers } )
-      .subscribe(data => {
-        console.log(data);
-      });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
 
   }
 }
