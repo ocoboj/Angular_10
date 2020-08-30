24b416a62b2b2235137fb877d0cf532dac5f8349
Olga Conesa <olgaconesaboj@gmail.com>
2020-08-30 10:17:22 +0200

    Pipe Personalizado: Capitalizar palabras

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   nombre: string = 'Olga Conesa';
+  nombre2: string = 'muJer maRavIlla';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
   porcentaje: number = 0.235;
