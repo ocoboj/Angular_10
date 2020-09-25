a9dbd5c72a5aa8f33797d856542562fbc24c907b
Olga Conesa <olgaconesaboj@gmail.com>
2020-09-25 09:38:38 +0200

    LocalStorage - Hacer persistente la información

--- 05-deseos/src/app/services/deseos.service.ts
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -11,11 +11,7 @@ export class DeseosService {
 
   constructor() {
 
-    const lista1 = new Lista('Comprar Tomates');
-    const lista2 = new Lista('Planchar la ropa');
-
-    this.listas.push(lista1, lista2);
-
+    this.cargarStorage();
 
    }
 
@@ -23,8 +19,23 @@ export class DeseosService {
 
     const nuevaLista = new Lista(titulo);
     this.listas.push( nuevaLista );
+    this.guardarStorage();
    }
 
+   guardarStorage(){
+
+    localStorage.setItem('data', JSON.stringify(this.listas));
+
+   }
+
+   cargarStorage(){
+
+    if ( localStorage.getItem('data')) {
+      this.listas = JSON.parse( localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
+   }
 
 
 }
